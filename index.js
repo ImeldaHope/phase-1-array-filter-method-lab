7d856676b89b3154a8c4de69ba801e0a3fbbdf0b
Imelda Hope <imeldalarusso@gmail.com>
2024-07-01 15:45:07 +0300

    creates functions that use .filter()

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+
+function findMatching(arr,str){
+    return arr.filter(driver => driver.toLowerCase() === str.toLowerCase()) 
+}
+
+findMatching()
+
+function fuzzyMatch(arr,str){
+    return arr.filter(name => name.slice(0,str.length) === str)
+}
+
+fuzzyMatch()
+
+function matchName(arr,str){
+    return arr.filter(param => param.name === str)
+}
+
+matchName()
